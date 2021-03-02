2d0084003fd8b514c0dea416d089fff5f5b2b9f8
cui250384093 <cui250384093@163.com>
2021-03-02 11:30:57 +0800

    test

--- src/com/yl/preparedstatement/crud/TransactionTest.java
+++ src/com/yl/preparedstatement/crud/TransactionTest.java
@@ -3,7 +3,9 @@ package com.yl.preparedstatement.crud;
 import com.yl.util.JDBCUtils;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * @author candk
@@ -54,7 +56,44 @@ public class TransactionTest {
         return 0;
     }
 
+    @Test
+    public void TransactionTest() throws Exception {
 
+        Connection conn = JDBCUtils.getConnection();
+
+    }
+
+    //session
+    public static <T> ArrayList<T> getInstances(Connection conn, Class<T> clazz, String sql, Object ...args) throws Exception {
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+        for (int i = 0; i < args.length; i++) {
+            ps.setObject(i + 1, args[i]);
+        }
+
+        ResultSet rs = ps.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        ArrayList<T> al =new ArrayList<T>();
+
+        while (rs.next()) {
+            T t = clazz.getDeclaredConstructor().newInstance();
+            for (int i = 0; i < columnCount; i++) {
+                Object columnValue = rs.getObject(i + 1);
+                String columnName = rsmd.getColumnLabel(i + 1);
+
+                Field field = clazz.getDeclaredField(columnName);
+                field.setAccessible(true);
+                field.set(t, columnValue);
+            }
+
+            al.add(t);
+        }
+
+        JDBCUtils.closeResource(null, ps, rs);
+
+        return al;
+    }
 
 
 }
